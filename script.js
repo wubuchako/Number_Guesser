8a4e3d088f5382692b57fa64f69b242c00f9de9a
wubuchako <56101474+wubuchako@users.noreply.github.com>
2020-02-12 18:09:40 +0900

    first commit
    created index.html and script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+/*
+GAME FUNCTION:
+ - Player must guess a number between a min and max
+- Player gets a certain amount of guesses
+- Notify the player of guesses remaining
+- Notify the player of the correct answer if loose 
+- Let player choose to play again
+ */
+
+
+//Game values
+let min = 1,
+    max = 10,
+    winningNum = 2,
+    guessesLeft = 3;
+
+// UI Elements
+const game = document.getElementById('game'), 
+minNum = document.querySelector('.min-num'),
+maxNum = document.querySelector('.max-num'),
+guessBtn = document.querySelector('#guess-btn'),
+guessInput = document.querySelector('#guess-input'),
+message = document.querySelector('.message');
+
+// assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+//Listen for guess
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+
+  //Validate
+  if(isNaN(guess) || guess < min || guess > max){
+    setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  }
+
+  //Check if won
+  if(guess === winningNum){
+    //disable input
+    guessInput.disabled = true;
+    //Change border color
+    guessInput.style.borderColor = 'green';
+    // Set message
+    setMessage(`${winningNum} is correct!, YOU WIN!`, 'green');
+
+  }else {
+
+  }
+});
+
+//Set message
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+}
